3f07f51a2c40530c120eb98d12a4dda63d8b4f76
zhouh <441237909@qq.com>
2022-08-18 17:29:00 +0800

    debug Mysql的调试，爬虫数据同时储存到mysql

--- src/main/java/com/zhj/esdemo/mapper/UserMapper.java
+++ src/main/java/com/zhj/esdemo/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.zhj.esdemo.mapper;
 
 import com.zhj.esdemo.pojo.User;
 import com.zhj.esdemo.pojo.UserBean;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -28,8 +29,11 @@ public interface UserMapper {
 //    List<UserBean> getUserList();
 
 
-//    //多个参数要加@Param修饰
-//    //增加，可以实现注册功能
-//    @Insert("insert into user(name,password)values(#{name},#{password})")
-//    void saveInfo(@Param("name") String name, @Param("password") String password);
+    //多个参数要加@Param修饰
+    //增加，可以实现注册功能
+    @Insert("insert into usertable(username,userpassword,age,sex)values(#{username},#{userpassword},#{age},#{sex})")
+    void saveInfo(@Param("username") String username, @Param("userpassword") String userpassword, @Param("age") Integer age, @Param("sex") String sex);
+
+    @Insert("insert into my_jd_goods(title,price,img)values(#{title},#{price},#{img})")
+    void addGoods(@Param("title") String title, @Param("price") String price, @Param("img") String img);
 }
--- src/test/java/com/zhj/esdemo/MysqlApiTests.java
+++ src/test/java/com/zhj/esdemo/MysqlApiTests.java
@@ -1,12 +1,16 @@
 package com.zhj.esdemo;
 
 import com.zhj.esdemo.mapper.UserMapper;
+import com.zhj.esdemo.pojo.Content;
 import com.zhj.esdemo.pojo.User;
 import com.zhj.esdemo.service.Impl.UserServiceImpl;
-import org.junit.Test;
+//import org.junit.Test;
+import com.zhj.esdemo.utils.Jdbc;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,8 +29,40 @@ public class MysqlApiTests {
 
 
     @Test
-    void mapperUse() {
+    void mapperUse() {   // 查询成功
         List<User> user = userMapper.getUserList();
+        String name = "张三";
+        String pwd = "123456";
+        User user2 = userMapper.getInfo("张三", "123456");
+    }
+
+    @Test
+    void inserUser() {
+        userMapper.saveInfo("王五", "123456", 18, "女");
+    }
+
+    @Test
+    void inserGoods() {
+
+        long stime = System.currentTimeMillis(); // 统计开始时间
+        List<Object> list = new ArrayList<>();
+        Content content = new Content();
+        content.setTitle("中国");
+//        content.get
+        content.setPrice("120");
+        content.setImg("hajajklsdfjl");
+        list.add(content);
+        list.add(content);
+        for (Object content1 : list) {
+            Content con = (Content) content1;
+            String title = con.getTitle();
+            String price = con.getPrice();
+            String img = con.getImg();
+            userMapper.addGoods(title, price, img);
+        }
+
+        long etime = System.currentTimeMillis(); // 统计结束时间
+        System.out.println("执行时间：" + (etime - stime));
     }
 
 //    @Test
